fe3cf152b958b7a7230cb27d7c4f83223141c997
dcouch440 <dcouch440@gmail.com>
2021-04-02 17:35:31 -0700

    add FastData module tests PASS

---
+++ spec/utils/fast_data_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe FastData do
+
+  describe 'Dogs#gen_dog' do
+
+    include FastData::Dogs
+
+    it 'creates a dog' do
+      gen_dog()
+      expect(Dog.count).to eq 1
+    end
+
+    it 'creates multiple dogs' do
+      10.times {gen_dog()}
+      expect(Dog.count).to eq 10
+    end
+
+    it 'returns a dog with defaults and changeable attributes' do
+      dog = gen_dog(name: 'Baby')
+      expect(dog.name).to eq 'Baby'
+    end
+
+  end
+
+  describe 'Cats#gen_cat' do
+
+    include FastData::Cats
+
+    it 'creates a cat' do
+      gen_cat()
+      expect(Cat.count).to eq 1
+    end
+
+    it 'creates multiple dogs' do
+      10.times {gen_cat()}
+      expect(Cat.count).to eq 10
+    end
+
+    it 'returns a cat with defaults and changeable attributes' do
+      cat = gen_cat(name: 'Sugar')
+      expect(cat.name).to eq 'Sugar'
+    end
+
+  end
+
+end

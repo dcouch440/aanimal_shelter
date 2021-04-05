db44228c39335ff07b3561f74360d4958292859f
dcouch440 <dcouch440@gmail.com>
2021-04-04 22:32:05 -0700

    refactor spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+
   include Response
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
@@ -8,4 +9,5 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid do |exception|
     json_response({ message: exception.message}, :unprocessable_entity)
   end
+
 end
